065a5ac01029b3139a23a1c5660e5d2dd38c3d22
cmj1980 <dagoclmt@gmail.com>
2022-05-09 16:14:00 -0700

    test commit fix

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -3,6 +3,8 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return (<li>{this.props.todo.name} {this.props.todo.completed?<span>-- Task Complete --</span> : <span></span>}</li>)
+    return (<li>{this.props.todo.name} {this.props.todo.completed?<span>-- Task Complete test --</span> : <span></span>}</li>)
   }
 }
+
+
